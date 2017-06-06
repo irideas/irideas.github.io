56bf55025e859e1cb5766bd47ab8d574ce882c9d
irideas <irideas@gmail.com>
2017-06-06 11:25:45 +0800

    Update test4close.js

--- test4close.js
+++ test4close.js
@@ -1,16 +1,37 @@
 function closeWindow() {
- var Browser = navigator.appName;
- var indexB = Browser.indexOf('Explorer');
+    var msie = (function() {
+        /**
+         * documentMode is an IE-only property
+         * http://msdn.microsoft.com/en-us/library/ie/cc196988(v=vs.85).aspx
+         */
+        if (typeof window.document.documentMode == "number") {
+            return window.document.documentMode;
+        }
+        var isLowIE = function(version) {
+            if (version >= 10) return false;
+            var b = window.document.createElement('b');
+            b.innerHTML = '<!--[if IE ' + (version || '') + ']><i></i><![endif]-->';
+            return b.getElementsByTagName('i').length === 1;
+        };
+        var ie;
+        for (var i = 9; i > 5; i--) {
+            if (isLowIE(i)) {
+                ie = i;
+                break;
+            }
+        }
+        return ie;
+    })(),
 
- if (indexB > 0) {
+ if (msie) {
     var indexV = navigator.userAgent.indexOf('MSIE') + 5;
     var Version = navigator.userAgent.substring(indexV, indexV + 1);
 
-    if (Version >= 7) {
+    if (msie >= 7) {
         window.open('about:blank', '_parent', '');
         window.close();
     }
-    else if (Version == 6) {
+    else if (msie == 6) {
         window.opener = null;
         window.close();
     }
