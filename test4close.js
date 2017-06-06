72b89bfbfee3f37011e4742d508da8692cc02008
irideas <irideas@gmail.com>
2017-06-06 11:05:48 +0800

    Update test4close.js

--- test4close.js
+++ test4close.js
@@ -1,44 +1,40 @@
-function closeWindows() {
-         var browserName = navigator.appName;
-         var browserVer = parseInt(navigator.appVersion);
-         //alert(browserName + " : "+browserVer);
+function closeWindow() {
+ var Browser = navigator.appName;
+ var indexB = Browser.indexOf('Explorer');
 
-         //document.getElementById("flashContent").innerHTML = "<br>&nbsp;<font face='Arial' color='blue' size='2'><b> You have been logged out of the Game. Please Close Your Browser Window.</b></font>";
+ if (indexB > 0) {
+    var indexV = navigator.userAgent.indexOf('MSIE') + 5;
+    var Version = navigator.userAgent.substring(indexV, indexV + 1);
 
-         if(browserName == "Microsoft Internet Explorer"){
-             var ie7 = (document.all && !window.opera && window.XMLHttpRequest) ? true : false;  
-             if (ie7)
-             {  
-               //This method is required to close a window without any prompt for IE7 & greater versions.
-               window.open('','_parent','');
-               window.close();
-             }
-            else
-             {
-               //This method is required to close a window without any prompt for IE6
-               this.focus();
-               self.opener = this;
-               self.close();
-             }
-        }else{  
-            //For NON-IE Browsers except Firefox which doesnt support Auto Close
-            try{
+    if (Version >= 7) {
+        window.open('', '_parent', '');
+        window.close();
+    }
+    else if (Version == 6) {
+        window.opener = null;
+        window.close();
+    }
+    else {
+        window.opener = '';
+        window.close();
+    }
+
+ }
+else {
+    //For NON-IE Browsers except Firefox which doesnt support Auto Close
+     try{
                 this.focus();
                 self.opener = this;
                 self.close();
             }
-            catch(e){
+     catch(e){}
 
-            }
-
-            try{
+      try{
                 window.open('','_self','');
                 window.close();
             }
-            catch(e){
+      catch(e){}
+   }
+}
 
-            }
-        }
-    }
-
-closeWindows();
+closeWindow();
